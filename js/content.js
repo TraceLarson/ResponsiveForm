f85e9ded6fe123b55396079c6364ca5334b43402
Trace Larson <trace.larson4917@gmail.com>
2017-09-18 00:48:33 -0500

    JS- cached the forms as constants.

--- js/content.js
+++ js/content.js
@@ -2,6 +2,12 @@
  * Created by tracelarson on 9/14/17.
  */
 
+//Order Form controls
+const orderDisplayShirt = document.querySelector('#order-display-shirt');
+const orderShirtPrice = document.querySelector('#order-shirt-price');
+const orderSelectSize = document.querySelector('#order-select-size');
+const orderSelectColor = document.querySelector('#order-select-color');
+
 //Shipping Details controls
 const shippingName = document.querySelector('#name-input');
 const shippingEmail = document.querySelector('#email-input');
@@ -11,4 +17,10 @@ const shippingCity = document.querySelector('#city-input');
 const shippingState = document.querySelector('#state-input');
 const shippingZip = document.querySelector('#zip-input');
 
-//Order Summary controls
+//Checkout Summary controls
+const checkoutProductName = document.querySelector('#checkout-product-name');
+const checkoutShipping = document.querySelector('#checkout-shipping');
+const checkoutTotal = document.querySelector('#checkout-total');
+const checkoutAddressName = document.querySelector('#checkout-address-name');
+const checkoutAddressOne = document.querySelector('#checkout-address1');
+const checkoutAddressCityStateZip = document.querySelector('#checkout-address-city-state-zip');
