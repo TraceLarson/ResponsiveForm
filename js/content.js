5115d4b688ec2e72064829b2a0bd4f5acaae2e44
Trace Larson <trace.larson4917@gmail.com>
2017-09-18 00:34:31 -0500

    integrating JS into HTML. adjusting order summary forms.

--- js/content.js
+++ js/content.js
@@ -1,3 +1,14 @@
 /**
  * Created by tracelarson on 9/14/17.
  */
+
+//Shipping Details controls
+const shippingName = document.querySelector('#name-input');
+const shippingEmail = document.querySelector('#email-input');
+const shippingAddressOne = document.querySelector('#address1-input');
+const shippingAddressTwo = document.querySelector('#address2-input');
+const shippingCity = document.querySelector('#city-input');
+const shippingState = document.querySelector('#state-input');
+const shippingZip = document.querySelector('#zip-input');
+
+//Order Summary controls
