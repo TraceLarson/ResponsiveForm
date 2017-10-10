ecb876cbaf90f531167fa3721de792e8fdc1f049
Trace Larson <trace.larson4917@gmail.com>
2017-10-09 23:00:58 -0500

    fixed javascript validation to use the type parameter. also used better regex pattern matching.

--- js/content.js
+++ js/content.js
@@ -3,8 +3,8 @@
  */
 //Order Form controls and event listeners
 window.onload = () => {
-	populateOrderSummary();
-	orderCompleted.setAttribute('class','hidden');
+    populateOrderSummary();
+    orderCompleted.setAttribute('class', 'hidden');
 };
 
 //Oder sections
@@ -14,17 +14,17 @@ const orderShirtImage = document.querySelector('#order-display-shirt');
 
 const orderSelectSize = document.getElementsByName('size');
 for (let i = 0; i < orderSelectSize.length; i++) {
-	orderSelectSize[i].addEventListener('click', () => {
-		populateOrderSummary();
-	});
+    orderSelectSize[i].addEventListener('click', () => {
+        populateOrderSummary();
+    });
 }
 
 const orderSelectColor = document.getElementsByName('color');
 for (let i = 0; i < orderSelectColor.length; i++) {
-	orderSelectColor[i].addEventListener('click', () => {
-		populateOrderSummary();
-		changeShirtImage(orderSelectColor[i].value);
-	});
+    orderSelectColor[i].addEventListener('click', () => {
+        populateOrderSummary();
+        changeShirtImage(orderSelectColor[i].value);
+    });
 }
 
 //Shipping Details controls
@@ -32,54 +32,54 @@ const shippingDetailsSection = document.getElementById('shipping-details')
 
 const shippingName = document.querySelector('#name-input');
 if (shippingName) {
-	shippingName.addEventListener('change', () => {
-		checkoutAddressName.innerHTML = shippingName.value;
-	});
+    shippingName.addEventListener('change', () => {
+        checkoutAddressName.innerHTML = shippingName.value;
+    });
 }
 
 const shippingEmail = document.querySelector('#email-input');
 
 const shippingAddressOne = document.querySelector('#address1-input');
 if (shippingAddressOne) {
-	shippingAddressOne.addEventListener('change', () => {
-		checkoutAddressOne.innerHTML = shippingAddressOne.value;
-	});
+    shippingAddressOne.addEventListener('change', () => {
+        checkoutAddressOne.innerHTML = shippingAddressOne.value;
+    });
 }
 
 const shippingAddressTwo = document.querySelector('#address2-input');
 if (shippingAddressTwo) {
-	shippingAddressTwo.addEventListener('change', () => {
-		checkoutAddressOne.innerHTML += ' , ' + shippingAddressTwo.value;
-	});
+    shippingAddressTwo.addEventListener('change', () => {
+        checkoutAddressOne.innerHTML += ' , ' + shippingAddressTwo.value;
+    });
 }
 
 const shippingCity = document.querySelector('#city-input');
 if (shippingCity) {
-	shippingCity.addEventListener('change', () => {
-		checkoutAddressCityStateZip.innerHTML = shippingCity.value + ', ';
-	});
+    shippingCity.addEventListener('change', () => {
+        checkoutAddressCityStateZip.innerHTML = shippingCity.value + ', ';
+    });
 }
 
 const shippingState = document.querySelector('#state-input');
 if (shippingState) {
-	shippingState.addEventListener('change', () => {
-		checkoutAddressCityStateZip.innerHTML += shippingState.value + ' ';
-	});
+    shippingState.addEventListener('change', () => {
+        checkoutAddressCityStateZip.innerHTML += shippingState.value + ' ';
+    });
 }
 
 const shippingZip = document.querySelector('#zip-input');
 if (shippingZip) {
-	shippingZip.addEventListener('change', () => {
-		checkoutAddressCityStateZip.innerHTML += shippingZip.value;
-	});
+    shippingZip.addEventListener('change', () => {
+        checkoutAddressCityStateZip.innerHTML += shippingZip.value;
+    });
 }
 
 const shippingCountry = document.querySelector('#country-select');
 console.log(shippingCountry.options[shippingCountry.selectedIndex].value);
-if(shippingCountry){
-	shippingCountry.addEventListener('change', () =>{
-		checkoutCountry.innerHTML = shippingCountry.options[shippingCountry.selectedIndex].value;
-	})
+if (shippingCountry) {
+    shippingCountry.addEventListener('change', () => {
+        checkoutCountry.innerHTML = shippingCountry.options[shippingCountry.selectedIndex].value;
+    })
 }
 
 //Checkout Summary controls
@@ -108,120 +108,109 @@ const error = document.getElementsByClassName('error');
 const orderCompleted = document.getElementById('order-completed');
 
 function getSize() {
-	for (let i = 0; i < orderSelectSize.length; i++) {
-		if (orderSelectSize[i].checked)
-			return orderSelectSize[i].value;
-	}
+    for (let i = 0; i < orderSelectSize.length; i++) {
+        if (orderSelectSize[i].checked)
+            return orderSelectSize[i].value;
+    }
 }
 
 function getColor() {
-	for (let i = 0; i < orderSelectColor.length; i++) {
-		if (orderSelectColor[i].checked)
-			return orderSelectColor[i].value;
-	}
+    for (let i = 0; i < orderSelectColor.length; i++) {
+        if (orderSelectColor[i].checked)
+            return orderSelectColor[i].value;
+    }
 }
 
 function populateOrderSummary() {
-	// checkoutProductName.value = getSize() + ' Basic V-Neck Tee ' + getColor();
-	checkoutProductName.innerHTML = getSize() + ' Basic V-Neck Tee ' + getColor();
+    // checkoutProductName.value = getSize() + ' Basic V-Neck Tee ' + getColor();
+    checkoutProductName.innerHTML = getSize() + ' Basic V-Neck Tee ' + getColor();
 
 }
 
 function changeShirtImage(color) {
-	orderShirtImage.setAttribute('src', 'images/' + color + '-v-neck.jpg');
-	orderShirtImage.setAttribute('alt', color + '-v-neck');
+    orderShirtImage.setAttribute('src', 'images/' + color + '-v-neck.jpg');
+    orderShirtImage.setAttribute('alt', color + '-v-neck');
 }
 
-function displaySuccess(){
-    orderCompleted.setAttribute('class','completed');
-    orderFormSection.setAttribute('class','hidden');
-    shippingDetailsSection.setAttribute('class','hidden');
-    checkoutSummarySection.setAttribute('class','hidden');
+function displaySuccess() {
+    orderCompleted.setAttribute('class', 'completed');
+    orderFormSection.setAttribute('class', 'hidden');
+    shippingDetailsSection.setAttribute('class', 'hidden');
+    checkoutSummarySection.setAttribute('class', 'hidden');
 
-    // completedProduct.innerHTML = checkoutProductName.value.toString();
     completedProduct.innerHTML = checkoutProductName.innerHTML;
-    // completedName.innerHTML =  checkoutAddressName.value.toString();
-    completedName.innerHTML =  checkoutAddressName.innerHTML;
-    // completedAddressOne.innerHTML = checkoutAddressOne.value.toString();
+    completedName.innerHTML = checkoutAddressName.innerHTML;
     completedAddressOne.innerHTML = checkoutAddressOne.innerHTML;
-    // completedAddressCityStateZip.innerHTML = checkoutAddressCityStateZip.value.toString();
     completedAddressCityStateZip.innerHTML = checkoutAddressCityStateZip.innerHTML;
-    // completedCountry.innerHTML = checkoutCountry.value.toString();
     completedCountry.innerHTML = checkoutCountry.innerHTML;
 
 }
 
-
 //Class to validate form inputs
 class IsValid {
-	constructor(input, type) {
-		this.input = input;
-		this.type = type;
-		this.errors = [];
-	}
+    constructor(input, type) {
+        this.input = input;
+        this.type = type;
+        this.errors = [];
+    }
 
-	addError(error) {
-		this.errors.push(error);
-	}
+    addError(error) {
+        this.errors.push(error);
+    }
 
-	getErrorMessages() {
-		const status = this.input.validity;
+    getErrorMessages() {
+        const status = this.input.validity;
 
-		if (status.valueMissing){
-			this.addError('This is a required field');
-		}
-		if (status.typeMismatch) {
-			this.addError('This entry does not match the field type.');
-		}
-		if (status.tooLong) {
-			this.addError('This entry is too long');
-		}
-		if (status.tooShort) {
-			this.addError('This entry is too short');
-		}
-
-		return this.errors;
-	}
+        if (status.valueMissing) {
+            this.addError('This is a required field');
+        }
+        if (status.typeMismatch) {
+            this.addError('This entry does not match the field type of ' + this.type + '.');
+        }
+        if (status.tooLong) {
+            this.addError('This entry is too long');
+        }
+        if (status.tooShort) {
+            this.addError('This entry is too short');
+        }
+        if (status.patternMismatch) {
+            this.addError('Field requirements not met');
+        }
+        return this.errors;
+    }
 
 }
 
 
 if (completeOrderButton) {
-	completeOrderButton.addEventListener('click', (event) => {
-		event.preventDefault();
+    completeOrderButton.addEventListener('click', (event) => {
+        event.preventDefault();
 
-		let validateArray = [shippingName, shippingEmail, shippingAddressOne, shippingCity, shippingState, shippingZip];
-		validateArray.forEach((control) => {
+        let validateArray = [shippingName, shippingEmail, shippingAddressOne, shippingCity, shippingState, shippingZip];
+        validateArray.forEach((control) => {
 
-			let validate = new IsValid(control, control.getAttribute('type'));
-			let errorMessages = validate.getErrorMessages();
+            let validate = new IsValid(control, control.getAttribute('type'));
+            let errorMessages = validate.getErrorMessages();
 
-			if(error){
-				let parentNode = control.parentNode;
-				for(let i = 0; i < parentNode.getElementsByTagName('p').length; i++) {
-					parentNode.removeChild(parentNode.getElementsByTagName('p')[i]);
-				}
-			}
-
-			if (errorMessages.length > 0) {
-				errorMessages.forEach((err) => {
-					control.insertAdjacentHTML('afterend', '<p class="error">' + err + '</p>');
-				})
-			}else if(errorMessages.length == 0 && error) {
-				//let parentNode = document.getElementsByClassName('shipping-info');
-				let parentNode = control.parentNode;
-				for (let i = 0; i < parentNode.getElementsByTagName('p').length; i++) {
-					parentNode.removeChild(parentNode.getElementsByTagName('p')[i]);
-				}
-			}
-			// else if(errorMessages.length == 0 && !error){
-			// 	console.log('hey');
-			// 	displaySuccess();
-			// }
-		});
-        if(error.length == 0){
+            if (error) {
+                let parentNode = control.parentNode;
+                for (let i = 0; i < parentNode.getElementsByTagName('p').length; i++) {
+                    parentNode.removeChild(parentNode.getElementsByTagName('p')[i]);
+                }
+            }
+            if (errorMessages.length > 0) {
+                errorMessages.forEach((err) => {
+                    control.insertAdjacentHTML('afterend', '<p class="error">' + err + '</p>');
+                })
+            } else if (errorMessages.length == 0 && error) {
+                let parentNode = control.parentNode;
+                for (let i = 0; i < parentNode.getElementsByTagName('p').length; i++) {
+                    parentNode.removeChild(parentNode.getElementsByTagName('p')[i]);
+                }
+            }
+        });
+        if (error.length == 0) {
             displaySuccess();
         }
-
-	});
+    });
 }
